8d001163f25cd22c87a1fa92971393f699f0e630
Tom Scott <tscott@weblinc.com>
2020-08-16 14:08:33 -0400

    Fix Rollup. Just wow.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,24 @@
+import babel from "@rollup/plugin-babel"
+import json from "@rollup/plugin-json"
+
 export default {
-    input: './src/index.js',
-    output: {
-        format: 'iife'
-    }
-};
+  input: "./src/index.js",
+  output: {
+    format: "iife",
+    file: "./build/benchwarmer.js"
+  },
+  plugins: [
+    babel({
+      exclude: "node_modules/**",
+      presets: [
+        [
+          "@babel/env",
+          {
+            modules: false
+          }
+        ]
+      ]
+    }),
+    json()
+  ]
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
 // Expose the OpenRCT2 to Visual Studio Code's Intellisense
 /// <reference path="OPENRCT2PATH/bin/openrct2.d.ts" />
 
-// Import a module from another file.
-import Message from "./module";
+import { version, author, license as licence } from "../package.json"
 
 function main() {
-    console.log(Message); // Display the imported "Hallo World" message from module.js.
+  ui.registerMenuItem("Add benches and trash cans to all paths", () => {
+    const paths = []
+    const bench = 0
+    const bin = 4
+
+    // Iterate every tile in the map
+    for (let y = 0; y < map.size.y; y++) {
+      for (let x = 0; x < map.size.x; x++) {
+        const tile = map.getTile(x, y)
+
+        // Iterate every element on the tile
+        for (let i = 0; i < tile.numElements; i++) {
+          const element = tile.getElement(i)
+
+          // If the element is a footpath, add it to our array
+          if (element.type === "footpath") {
+              paths.push(element)
+          }
+        }
+      }
+    }
+
+    paths.forEach((path, index) => {
+      if (index % 2 === 0) {
+        path.addition = bench
+      } else {
+        path.addition = bin
+      }
+    })
+  })
 }
 
 registerPlugin({
-    name: "MYPLUGINNAME",
-    version: "0.1",
-    licence: "MIT", // Make sure to set the license prior to release
-    authors: [""],
+    name: "Benchwarmer",
+    version,
+    licence,
+    authors: [author],
     type: "local",
     main: main
-});
+})
