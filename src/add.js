8474aeabdc1d513632ba1f5d60121af4b1c9f687
Tom Scott <tscott@weblinc.com>
2020-08-16 14:42:49 -0400

    refactor

---
+++ src/add.js
@@ -0,0 +1,28 @@
+export default function Add(bench=0, bin=4) {
+  const paths = []
+
+  // Iterate every tile in the map
+  for (let y = 0; y < map.size.y; y++) {
+    for (let x = 0; x < map.size.x; x++) {
+      const tile = map.getTile(x, y)
+
+      // Iterate every element on the tile
+      for (let i = 0; i < tile.numElements; i++) {
+        const element = tile.getElement(i)
+
+        // If the element is a footpath, add it to our array
+        if (element.type === "footpath") {
+            paths.push(element)
+        }
+      }
+    }
+  }
+
+  paths.forEach((path, index) => {
+    if (index % 2 === 0) {
+      path.addition = bench
+    } else {
+      path.addition = bin
+    }
+  })
+}
--- src/index.js
+++ src/index.js
@@ -2,42 +2,16 @@
 /// <reference path="OPENRCT2PATH/bin/openrct2.d.ts" />
 
 import { version, author, license as licence } from "../package.json"
+import Add from "./add"
+
+const name = "Benchwarmer"
 
 function main() {
-  ui.registerMenuItem("Add benches and trash cans to all paths", () => {
-    const paths = []
-    const bench = 0
-    const bin = 4
-
-    // Iterate every tile in the map
-    for (let y = 0; y < map.size.y; y++) {
-      for (let x = 0; x < map.size.x; x++) {
-        const tile = map.getTile(x, y)
-
-        // Iterate every element on the tile
-        for (let i = 0; i < tile.numElements; i++) {
-          const element = tile.getElement(i)
-
-          // If the element is a footpath, add it to our array
-          if (element.type === "footpath") {
-              paths.push(element)
-          }
-        }
-      }
-    }
-
-    paths.forEach((path, index) => {
-      if (index % 2 === 0) {
-        path.addition = bench
-      } else {
-        path.addition = bin
-      }
-    })
-  })
+  ui.registerMenuItem(name, () => { Add() })
 }
 
 registerPlugin({
-    name: "Benchwarmer",
+    name,
     version,
     licence,
     authors: [author],
