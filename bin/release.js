2295ff400babb4c3b42929e50b669004ad4d5f05
Tom Scott <tscott@weblinc.com>
2020-08-16 18:02:50 -0400

    Add release task

--- bin/release.js
+++
@@ -1,6 +0,0 @@
-const config = require("../package.json")
-const tar = require("tar")
-const file = `${config.name}-${config.version}.tar.gz`
-
-tar.create({ gzip: true, file }, ["build/benchwarmer.js"])
-   .then(function() { console.log("Tarball has been created") })
