123cc41e2446887437234a1ec275efdf8366f531
Tom Scott <tscott@nerdstgamers.com>
2021-08-15 14:12:49 -0400

    test(add): test basic functionality

---
+++ src/add.test.ts
@@ -0,0 +1,33 @@
+import Add from "./add";
+import Settings from "./settings";
+import { mocked } from "ts-jest/utils";
+
+const sharedStorage = mocked(context.sharedStorage);
+
+test("add benches and bins", () => {
+  sharedStorage.get.mockReturnValue(0);
+  const all: LoadedObject[] = [
+    {
+      index: 0,
+      type: "footpath_addition",
+      identifier: "bench",
+      legacyIdentifier: "bench",
+      name: "bench",
+    },
+    {
+      index: 1,
+      type: "footpath_addition",
+      identifier: "litter",
+      legacyIdentifier: "litter",
+      name: "litter bin",
+    },
+  ];
+  const [bench, bin] = all;
+  const settings = new Settings(all);
+  const { sloped, unsloped } = Add(settings);
+  const paths = [...sloped, ...unsloped].map(({ path }) => path);
+
+  expect(paths).not.toHaveLength(0);
+  expect(paths[1].addition).toEqual(bench.index);
+  expect(paths[2].addition).toEqual(bin.index);
+});
