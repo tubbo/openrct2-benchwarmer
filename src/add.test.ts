7170f7baae236bc3dba4f9b0b1670e4224280cfc
Ian-Polito <ipolito@mailfence.com>
2021-10-28 01:52:53 -0400

    added new test cases in settings.test tried adding new test in add.test for adding queue tvs couldn't get it to work, left it commented. it does work though

--- src/add.test.ts
+++ src/add.test.ts
@@ -1,4 +1,5 @@
 import { Add } from "./add";
+//import { AddQueueTVs } from "./add";
 import { Settings } from "./settings";
 import { mocked } from "ts-jest/utils";
 
@@ -33,5 +34,29 @@ describe("add", () => {
     expect(paths).not.toHaveLength(0);
     expect(paths[1].addition).toStrictEqual(bench.index);
     expect(paths[2].addition).toStrictEqual(bin.index);
-  });
+  })/*,
+  //Couldn't figure out how to get this test case to work
+  it("places queue tvs", () => {
+    expect.hasAssertions();
+    sharedStorage.get.mockReturnValue(0);
+
+    const all: LoadedObject[] = [
+      {
+        index: 0,
+        type: "footpath_addition",
+        identifier: "rct2.qtv1",
+        legacyIdentifier: "qtv1",
+        name: "queue tv",
+      },
+    ];
+    const [queuetv] = all;
+    const settings = new Settings(all);
+    const { queue } = AddQueueTVs(settings);
+    const paths = [...queue].map(({ path }) => path);
+
+    //paths always comes up as empty, not sure why
+    expect(paths).not.toHaveLength(0);
+    expect(paths[1].addition).toStrictEqual(queuetv.index);
+  })
+  */
 });
